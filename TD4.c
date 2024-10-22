e99c11c3a3288be780be7ad99764c261d838d53b
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-10-22 12:24:24 +0200

    Le TP4 est good (je crois (non))

--- TD4.c
+++ TD4.c
@@ -52,37 +52,56 @@ int main(){
 
 // Question 2 :
 
-// Fonction appelée quand l'alarme sonne
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+// Structure sigaction
+struct sigaction sig_abandon;
+
 void msgAlarm(int sig){
-    printf("\nAlors ça vient ?\n"); // Affiche un message
-    alarm(15); // Relance l'alarme pour 15 secondes
+    printf("\nAlors ça vient ?\n");$
+    // odification du signal 
+    sig_abandon.sa_handler = msgAlarm2;
+    sig_abandon.sa_flags = SA_RESETHAND;
+    sigemptyset(&sig_abandon.sa_mask);
+    sigaction(SIGALRM, &sig_abandon, NULL);
+    alarm(30);
 }
+
+
+void msgAlarm2(int sig){
+    printf("\nBon j'abandonne !\n");
+    exit(0);
+}  
+
 
 int main(){
     // Variables
     char c;
     struct sigaction sig_relance, ancien;
-    sig_relance.sa_handler = msgAlarm; // Associe la fonction msgAlarm au signal
-    sig_relance.sa_flags = SA_RESTART; // Redémarre les appels système interrompus
-    sigemptyset(&sig_relance.sa_mask); // Vide le masque de signaux
+    sig_relance.sa_handler = msgAlarm;
+    sig_relance.sa_flags = SA_RESTART;
+    sigemptyset(&sig_relance.sa_mask);
 
     // Créer le signal
-    sigaction(SIGALRM, &sig_relance, &ancien); // Configure le signal SIGALRM
+    sigaction(SIGALRM, &sig_relance, &ancien);
 
     // Créer l'alarme
-    alarm(15); // Lance une alarme pour 15 secondes
+    alarm(15);
 
     // Attendre la saisie utilisateur
     printf("Entrez quelque chose : ");
-    scanf("%c", &c); // Attend que l'utilisateur entre un caractère
+    scanf("%c", &c);
 
     // Annuler l'alarme
-    alarm(0); // Annule l'alarme
+    alarm(0);
 
     // Simuler un calcul
     printf("Traitement en cours...\n");
-    sigaction(SIGALRM, &ancien, NULL); // Restaure l'ancienne action du signal
-    sleep(10); // Attend 10 secondes
+    sigaction(SIGALRM, &ancien, NULL);
+    sleep(10);
 }
 
 // Question 3 :
@@ -91,36 +110,97 @@ int main(){
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-// Fonction appelée quand l'alarme sonne
-void msgAlarm(int sig){
-    printf("\nAlors ça vient ?\n"); // Affiche un message
-    alarm(15); // Relance l'alarme pour 15 secondes
+#define SIZE 1000000  // Taille du tableau
+#define TARGET 123456 // Élément à rechercher
+
+// Variable globale pour le fils afin de savoir s'il doit interrompre sa recherche
+volatile sig_atomic_t arret_fils = 0;
+
+// Gestionnaire du signal SIGUSR1
+void handle_sigusr1(int sig) {
+    arret_fils = 1;  // Le signal indique d'arrêter la recherche
 }
 
-int main(){
-    // Variables
-    char c;
-    struct sigaction sig_relance, ancien;
-    sig_relance.sa_handler = msgAlarm; // Associe la fonction msgAlarm au signal
-    sig_relance.sa_flags = SA_RESTART; // Redémarre les appels système interrompus
-    sigemptyset(&sig_relance.sa_mask); // Vide le masque de signaux
+// Fonction de recherche utilisée par le fils
+bool search_fils(int *arr, int start, int end, const char *who) {
+    for (int i = start; i < end; i++) {
+        if (arret_fils) {
+            // Si le signal a été reçu, le fils arrête la recherche
+            printf("%s a été interrompu\n", who);
+            return false;
+        }
 
-    // Créer le signal
-    sigaction(SIGALRM, &sig_relance, &ancien); // Configure le signal SIGALRM
+        if (arr[i] == TARGET) {
+            printf("%s a trouvé en position : %d\n", who, i);
+            return true;
+        }
+    }
+    return false;
+}
 
-    // Créer l'alarme
-    alarm(15); // Lance une alarme pour 15 secondes
+bool search_pere(int *arr, int start, int end, const char *who) {
+    for (int i = start; i < end; i++) {
+        if (arr[i] == TARGET) {
+            printf("%s a trouvé en position : %d\n", who, i);
+            return true;
+        }
+    }
+    return false;
+}
 
-    // Attendre la saisie utilisateur
-    printf("Entrez quelque chose : ");
-    scanf("%c", &c); // Attend que l'utilisateur entre un caractère
+int main() {
+    int *arr = malloc(SIZE * sizeof(int));
+    if (arr == NULL) {
+        perror("Erreur d'allocation de mémoire");
+        exit(EXIT_FAILURE);
+    }
 
-    // Annuler l'alarme
-    alarm(0); // Annule l'alarme
+    // Initialisation du tableau avec des valeurs quelconques
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = i;
+    }
 
-    // Simuler un calcul
-    printf("Traitement en cours...\n");
-    sigaction(SIGALRM, &ancien, NULL); // Restaure l'ancienne action du signal
-    sleep(10); // Attend 10 secondes
+    pid_t pid = fork();  // Création d'un processus fils
+
+    if (pid < 0) {
+        perror("Erreur lors de la création du processus");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        // Code du processus fils : il explore la 2ème moitié du tableau
+        signal(SIGUSR1, handle_sigusr1);  // Installation du gestionnaire de signal
+
+        bool fils_trouve = search_fils(arr, SIZE / 2, SIZE, "Le fils");
+
+        if (fils_trouve) {
+            // Si le fils trouve l'élément, il sort
+            exit(EXIT_SUCCESS);
+        } else {
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // Code du processus père : il explore la 1ère moitié du tableau
+        bool pere_trouve = search_pere(arr, 0, SIZE / 2, "Le père");
+
+        if (pere_trouve) {
+            // Si le père trouve l'élément, il envoie SIGUSR1 au fils
+            kill(pid, SIGUSR1);
+        }
+
+        // Le père attend que le fils termine
+        wait(NULL);
+
+        // Si ni le père ni le fils n'ont trouvé l'élément
+        if (!pere_trouve) {
+            printf("L'élément n'a pas été trouvé\n");
+        }
+    }
+
+    free(arr);  // Libération de la mémoire
+    return 0;
 }
